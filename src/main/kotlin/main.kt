9cc78cbbfe1875d4e54c1ff267c8e98bff7b34ab
Andrey Lobanov <andrey922gmail.com>
2022-11-11 01:37:03 +0300

    camelCase fixed

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,8 @@
 class Likes(
     count: Int = 0,//число пользователей, которым понравилась запись
     val userLikes: Boolean = false, //наличие отметки «Мне нравится» от текущего пользователя
-    val can_like: Boolean = false, //может ли текущий пользователь поставить отметку «Мне нравится»
-    val can_publish: Boolean = false //может ли текущий пользователь сделать репост записи
+    val canLike: Boolean = false, //может ли текущий пользователь поставить отметку «Мне нравится»
+    val canPublish: Boolean = false //может ли текущий пользователь сделать репост записи
 ) {
     var count: Int = count
         set(value) {
@@ -12,10 +12,10 @@ class Likes(
 
 class Comments(
     count: Int = 0, // количество комментариев
-    val can_post: Boolean = false, // может ли текущий пользователь комментировать запись
-    val groups_can_post: Boolean = false, // могут ли сообщества комментировать запись
-    val can_close: Boolean = false, // может ли текущий пользователь закрыть комментарии к записи
-    val can_open: Boolean = false // может ли текущий пользователь открыть комментарии к записи
+    val canPost: Boolean = false, // может ли текущий пользователь комментировать запись
+    val groupsCanPost: Boolean = false, // могут ли сообщества комментировать запись
+    val canClose: Boolean = false, // может ли текущий пользователь закрыть комментарии к записи
+    val canOpen: Boolean = false // может ли текущий пользователь открыть комментарии к записи
 ) {
     var count: Int = count
         set(value) {
@@ -25,17 +25,17 @@ class Comments(
 
 data class Post(
     val id: Int = 1, // Идентификатор записи
-    val owner_id: Int = 1, // Идентификатор владельца стены, на которой размещена запись
-    val from_id: Int = 2, //Идентификатор автора записи
+    val ownerId: Int = 1, // Идентификатор владельца стены, на которой размещена запись
+    val fromId: Int = 2, //Идентификатор автора записи
     val date: Int = 1590075360, // Время публикации записи в формате unixtime
     val text: String = "New empty post", // Текст записи
-    val friends_only: Boolean = false, // true, если запись была создана с опцией «Только для друзей»
+    val friendsOnly: Boolean = false, // true, если запись была создана с опцией «Только для друзей»
     val comments: Comments, // Комментарии к записи
     val likes: Likes, // Информация о лайках к записи
-    val post_type: String = "post", // Тип записи, может принимать следующие значения: post, copy, reply, postpone, suggest
-    val can_pin: Boolean = false, // Может ли текущий пользователь закрепить запись
-    val can_delete: Boolean = false, // Может ли текущий пользователь удалить запись
-    val can_edit: Boolean = false, // Может ли текущий пользователь редактировать запись
+    val postType: String = "post", // Тип записи, может принимать следующие значения: post, copy, reply, postpone, suggest
+    val canPin: Boolean = false, // Может ли текущий пользователь закрепить запись
+    val canDelete: Boolean = false, // Может ли текущий пользователь удалить запись
+    val canEdit: Boolean = false, // Может ли текущий пользователь редактировать запись
 )
 
 object WallService {
@@ -50,7 +50,7 @@ object WallService {
     fun update(changedPost: Post): Boolean {
         for ((index, post) in posts.withIndex()) {
             if (changedPost.id == post.id) {
-                posts[index] = changedPost.copy(owner_id = post.owner_id, date = post.date)
+                posts[index] = changedPost.copy(ownerId = post.ownerId, date = post.date)
                 return true
             }
         }
@@ -69,7 +69,7 @@ object WallService {
 fun main() {
     val likes = Likes()
     val comments = Comments()
-    val myPost = Post(id = 1, likes = likes, comments = comments, post_type = "post")
+    val myPost = Post(id = 1, likes = likes, comments = comments)
     println(myPost)
 
     likes.count = 10
